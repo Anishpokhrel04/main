862225ec2db248e6a5152b60fe0ed0958942919d
Anishpokhrel04 <anishpokhrel234@gmail.com>
2022-12-06 19:25:10 +0545

    ..

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ import TextForm from './Components/TextForm';
 
 function App(props) {
   const[mode ,setMode ]= useState('light')//wheter to enable dark mode or not
+  
   const toggleMode=() =>{
     if(mode==="light"){
       setMode("dark");
       document.body.style.backgroundColor ="#042743";
+      
     }
+    
     else{
       setMode("light");
       document.body.style.backgroundColor ="white";
+     
     }
   }
   return (
